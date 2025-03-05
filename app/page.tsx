12096cdd6b2f8a0961790049f8b1509b242eb344
Cliff <freemancliff@icloud.com>
2025-03-05 09:17:54 -0500

    First Site

--- app/page.tsx
+++ app/page.tsx
@@ -5,30 +5,33 @@ export default function Example() {
         <div className="px-6 pt-10 pb-24 sm:pb-32 lg:col-span-7 lg:px-0 lg:pt-40 lg:pb-48 xl:col-span-6">
           <div className="mx-auto max-w-lg lg:mx-0">
             <img
-              alt="Your Company"
+              alt="Test Photo"
               src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
               className="h-11"
             />
             <div className="hidden sm:mt-32 sm:flex lg:mt-16">
               <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-500 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                Anim aute id magna aliqua ad ad non deserunt sunt.{' '}
-                <a href="#" className="font-semibold whitespace-nowrap text-indigo-600">
+                Welcome to my site. Press this button to{" "}
+                <a
+                  href="#"
+                  className="font-semibold whitespace-nowrap text-red-700"
+                >
                   <span aria-hidden="true" className="absolute inset-0" />
                   Read more <span aria-hidden="true">&rarr;</span>
                 </a>
               </div>
             </div>
             <h1 className="mt-24 text-5xl font-semibold tracking-tight text-pretty text-gray-900 sm:mt-10 sm:text-7xl">
-              Data to enrich your business
+              Cliff Freeman
             </h1>
             <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat
+              My name is Cliff Freeman. Something that I did today was drink
+              three cups of coffee.
             </p>
             <div className="mt-10 flex items-center gap-x-6">
               <a
                 href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-md bg-red-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-red-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-700"
               >
                 Get started
               </a>
@@ -41,11 +44,11 @@ export default function Example() {
         <div className="relative lg:col-span-5 lg:-mr-8 xl:absolute xl:inset-0 xl:left-1/2 xl:mr-0">
           <img
             alt=""
-            src="https://images.unsplash.com/photo-1487017159836-4e23ece2e4cf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2102&q=80"
-            className="aspect-3/2 w-full bg-gray-50 object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full"
+            src="https://photos.smugmug.com/Photography/i-SX7tjL5/0/MGHmLSSsCdzcTQdj23BLHgbH48vJ43wpDLR4xLFxt/X4/02042023_WhiteSpaces_HR%203-X4.jpg"
+            className="w-full h-full object-contain lg:absolute lg:inset-0"
           />
         </div>
       </div>
     </div>
-  )
+  );
 }
